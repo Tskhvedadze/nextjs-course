3bdbd34ad6dbcd20fb14376183822b6645834beb
Tskhvedadze <giorgicxvedadze4@gmail.com>
2023-09-06 15:09:37 +0400

    more dynamic files and folders

---
+++ pages/client/index.js
@@ -0,0 +1,9 @@
+function ClientPage() {
+  return (
+    <div>
+      <h1>The client page</h1>
+    </div>
+  );
+}
+
+export default ClientPage;
---
+++ pages/portfolio/index.js
@@ -0,0 +1,9 @@
+function Portfoliopage() {
+  return (
+    <div>
+      <h1>The portfolio page</h1>
+    </div>
+  );
+}
+
+export default Portfoliopage;
