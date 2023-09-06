8122c095aa425541c7001d1536183b5d69a80a27
Tskhvedadze <giorgicxvedadze4@gmail.com>
2023-09-06 16:01:54 +0400

    completed this chapter

--- pages/client/[id]/index.js
+++ pages/client/[id]/index.js
@@ -5,9 +5,20 @@ function ClientProjectsPage() {
 
   console.log(router.query);
 
+  function loadProjectHandler() {
+    router.push({
+      pathname: "/client/[id]/[clientprojects]",
+      query: {
+        id: "max",
+        clientprojects: "projectA",
+      },
+    });
+  }
+
   return (
     <div>
       <h1>The Projects of a Given Clients</h1>
+      <button onClick={loadProjectHandler}>Load Project A</button>
     </div>
   );
 }
--- pages/client/index.js
+++ pages/client/index.js
@@ -1,7 +1,36 @@
+import Link from "next/link";
+
 function ClientPage() {
+  const clients = [
+    { id: "max", name: "Maximilian" },
+    { id: "manu", name: "Manuel" },
+  ];
+
   return (
     <div>
       <h1>The client page</h1>
+      {/* <ul>
+        <li>
+          <Link href="client/max">Maximilian</Link>
+        </li>
+        <li>
+          <Link href="client/manuel">Manuel</Link>
+        </li>
+      </ul> */}
+      {clients.map((client) => (
+        <ul key={client.id}>
+          <li>
+            <Link
+              href={{
+                pathname: "/client/[id]",
+                query: { id: client.id },
+              }}
+            >
+              {client.name}
+            </Link>
+          </li>
+        </ul>
+      ))}
     </div>
   );
 }
